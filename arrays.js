f2fcb6a67c28e30aa2fe23a0f431573700e76798
Bret Hogg <hoggb@wwu.edu>
2017-11-03 06:01:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(arr, value){
-  
+  arr = [value, ...arr]
+  return arr
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, value){
-  arr.unshift(value)
+  return arr.unshift(value)
+  
 }
